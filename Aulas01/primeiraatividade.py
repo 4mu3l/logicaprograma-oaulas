4a26ef48bebeeb1dc674ed3c9e3eb12ffea9149e
Samuel dos Santos Rodrigues <samuel.rodigues@sesidf.org.br>
2025-08-07 12:10:19 -0300

    1

---
+++ Aulas01/primeiraatividade.py
@@ -0,0 +1,39 @@
+nome ="Samuel"
+idade = 16
+nasc= 2008
+print(30*"-","concatenando o texto",30*"-")
+print("Oi, eu sou o",nome , "minha idade é",idade, "pois nasci em ",nasc)
+print(30*"-","concatenando o texto",30*"-")
+#variaveis
+idade= 25
+altura = 1.75
+ativo = True 
+print("O tipo de varivel nome é",type(nome))
+print("O tipo de varivel idade é",type(idade))
+print("O tipo de varivel altura é",type (altura))
+print("O tipo de varivel ativo é",type (ativo))
+print(30*"-")
+#Operadores
+num1= 10
+num2 = 10
+soma = num1 + num2
+divi = num1 / num2
+mult = num1 * num2
+sub = num1 - num2 
+print("Oresultado da mult é:", mult)
+print(30*"-")
+#operadores de atribuíção
+num1 > num2
+num1 < num2
+num1 == num2
+num1>= num2
+num1 <= num2
+num1 != num2
+ano = 2025
+print("ano atual", ano)
+ano += 1
+print ('ano acrecido de +1', ano)
+ano -= 1
+print("ano acrecido de -1", ano)
+print(30*"-")
+
