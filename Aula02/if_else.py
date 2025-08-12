38afd01a4fe9170e53d54b492694abdd91334a2d
Samuel dos Santos Rodrigues <samuel.rodigues@sesidf.org.br>
2025-08-12 12:01:02 -0300

    aula dia 12

--- Aula02/if_else.py
+++ Aula02/if_else.py
@@ -5,7 +5,7 @@
 # if else
 # operador ternario
 # FIXME - concatenação com +
-
+'''
 nome = "Gomes"
 idade = 26
 altura = 1.75
@@ -34,5 +34,66 @@ print(30*"-")
 peso = input("digite seu peso: ").replace(",",".")
 peso = float(peso)
 print(f'Esse peso é o seu mesmo {peso}'.replace(".",","))
+print(30*"-")
+#variaveis
+nome= input('digite seu nome ')
+idade= int(input('digite sua idade '))
+#estrutura de decisão
+if idade >= 18:
+    print(f'{nome} é maior de idade.')
+else:
+    print(f'{nome} é menor de idade')
+print(50*"-")
+#BOLETIM ESCOLAR COM IF, ELSE E ELIF
+print(40*"-","BOLETIM ESCOLAR", 40*"-")
+nome_aluno = input("Digite o nome do aluno ")
+nota1 = float(input("Digite sua nota "))
+nota2 = float(input("Digite sua nota "))
+nota3 = float(input("Digite sua nota "))
+nota4 = float(input("Digite sua nota "))
+media = (nota1+nota2+nota3+nota4)/4
+#>=7: aprovado
+#>=5: recuperação
+#reprovado
+if media >=7:
+    print(f'{nome_aluno} aluno aprovado!')
+    print(f'nota 1; {nota1}| nota 2: {nota2}')
+    print(f'nota 3: {nota3}| nota 4: {nota4}')
+    print(20*'-')
+    print(f"media: {media}")
+elif media >=5:
+    print(f'{nome_aluno} aluno recuperação!')
+    print(f'nota 1:{nota1}| nota 2: {nota2}')
+    print(f'nota 3:{nota3}| nota 4: {nota4}')
+    print(20*'-')
+    print(f"media: {media}")
+else:
+    print(f'{nome_aluno} aluno reprovado!')
+    print(f'nota 1{nota1}| nota 2: {nota2}')
+    print(f'nota 3{nota3}| nota 4: {nota4}')
+    print(20*'-')
+    print(f"media: {media}")
+'''
+'''
+#MONTANHA RUSSA
+#variaveis
+nome = input("Digite seu nome ")
+idade = float(input("Digite sua idade "))
+altura = float(input("Digite sua altura ").replace(",","."))
+altura =float(altura)
+# verificação dos requsitos
+if idade >= 12 and altura>= 1.2:
+    print(f"É concedido a entrada de {nome} para a montanha russa")
+else:
+    print(f"A permissão para a entrada recusada, melhore!!!")
+'''
+# variaveis
+nome= 'alex'
+idade = 19
+print(nome,'maior de idade,' if idade >= 18 else 'é menor de idade')
+
+    
+
+
 
 
