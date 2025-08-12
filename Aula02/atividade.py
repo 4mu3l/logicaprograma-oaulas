4aa70210700e88498a3b9d0761d38d17e261efaf
Samuel dos Santos Rodrigues <samuel.rodigues@sesidf.org.br>
2025-08-12 09:23:30 -0300

    atividade 1

---
+++ Aula02/atividade.py
@@ -0,0 +1,11 @@
+# atvidade 1: criar variaveis com os tipos int, float, str e bool e imprima seus valores
+#variavel int
+idade = 13
+#variavel float
+peso = 54.6
+#variavel str
+nome = "Samuel"
+#variavel bool
+sim = True
+# imprimindo os valores
+print(f"A idade é {13}, o peso é {54.6} e no true ou false é {sim}")
